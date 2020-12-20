62c706419729968898aef036cace26af213545d7
Ana Julia Volpi <premiersoft-ana.volpi@ailos.coop.br>
2020-12-20 17:03:52 -0300

    Adding more features into the game

--- programacao_orientada_objetos/ProjetoFinal_AnaJulia_Nicole/src/Jogo.java
+++ programacao_orientada_objetos/ProjetoFinal_AnaJulia_Nicole/src/Jogo.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 
 public class Jogo {
-	
+
 	private Tabuleiro tabuleiro;
 	boolean terminar = false;
 	boolean ganhar = false;
 	int[] jogada;
+	Scanner scanner = new Scanner(System.in);
+
 
 	public Jogo(){
 		tabuleiro = new Tabuleiro();
@@ -12,26 +15,137 @@ public class Jogo {
 		jogada = new int[2];
 	}
 
+//	public void Jogar(Tabuleiro tabuleiro){
+//		
+//		do{
+//			
+//			tabuleiro.mostrarTabuleiro();
+//			terminar = tabuleiro.setPosicao(); //PERDEU: diz se a posicao tem -1 que é quando o jogo acaba
+//
+//			if(!terminar){ // if FALSE 
+//				tabuleiro.abrirVizinhas();
+//				terminar = tabuleiro.ganharJogo();
+//			}
+//
+//		}while(!terminar);
+//
+//		if(!tabuleiro.ganharJogo()){
+//			System.out.println("Voce PERDEU!!");
+//			tabuleiro.exibirTabuleiro();
+//			tabuleiro.salvarHistorico(0);
+//			tabuleiro.lerHistorico();
+////			tabuleiro.limparHistorico();
+//		} else {
+//			System.out.println("Voce GANHOU!");
+//			tabuleiro.exibirTabuleiro();
+//			tabuleiro.salvarHistorico(1);
+//			tabuleiro.lerHistorico();
+////			tabuleiro.limparHistorico();
+//		}		
+//	}	
+	
 	public void Jogar(Tabuleiro tabuleiro){
-		
-		do{
-			
-			tabuleiro.mostrarTabuleiro();
-			terminar = tabuleiro.setPosicao(); //PERDEU: diz se a posicao tem -1 que é quando o jogo acaba
 
-			if(!terminar){ // if FALSE 
-				tabuleiro.abrirVizinhas();
-				terminar = tabuleiro.ganharJogo();
+		int op = -1;
+//		do{
+		while(!terminar && op != 0) {
+
+//			int op = -1;
+//			while(op != 0){
+				op = menuPrincipal();
+				switch(op){
+				case 1:
+
+//					int nivel = escolherNivel();
+//					switch(nivel){
+//					case 1:
+
+					do {
+						tabuleiro.mostrarTabuleiro();
+						terminar = tabuleiro.setPosicao(); //PERDEU: diz se a posicao tem -1 que é quando o jogo acaba
+
+						if(!terminar){ // if FALSE 
+							tabuleiro.abrirVizinhas();
+							terminar = tabuleiro.ganharJogo();
+						}
+					}while(!terminar);				
+					
+						break;
+//					}
+				case 2:
+					tabuleiro.lerHistorico();
+					break;	
+				case 3:
+					tabuleiro.limparHistorico();
+					break;
+				case 4:
+					mostrarCreditos();
+					break;
+				case 0:
+					System.out.println("Finalizando o sistema");
+					break;
+				default:
+					System.out.println("Opção inválida");
+					break;
+//				}
 			}
 
-		}while(!terminar);
 
-		if(!tabuleiro.ganharJogo()){
-			System.out.println("Voce PERDEU!!");
-			tabuleiro.exibirTabuleiro();
-		} else {
-			System.out.println("Voce GANHOU!");
-			tabuleiro.exibirTabuleiro();
+//		}while(!terminar && op != 0);
 		}
+				
+		if (op != 0) {	
+			if(!tabuleiro.ganharJogo()){
+				System.out.println("\nVoce PERDEU!!\n");
+				tabuleiro.exibirTabuleiro();
+				tabuleiro.salvarHistorico(0);
+			} else {
+				System.out.println("\nVoce GANHOU!!\n");
+				tabuleiro.exibirTabuleiro();
+				tabuleiro.salvarHistorico(1);
+			}	
+		}
+	}
+
+
+	private void mostrarCreditos() {
+		System.out.println("\n\n");
+		System.out.println(" -------------------------------------");
+		System.out.println("|                                     |");
+		System.out.println("|   Este jogo foi desenvolvido por:   |");
+		System.out.println("|                                     |");
+		System.out.println("|          Ana  Julia  Volpi          |");
+		System.out.println("|           Nicole  Bortoli           |");
+		System.out.println("|                                     |");
+		System.out.println("|             IFSC Gaspar             |");
+		System.out.println("|                                     |");
+		System.out.println(" -------------------------------------");
+		System.out.println("\n\n");
+}
+
+	public int menuPrincipal(){
+		System.out.println("\n");
+		System.out.println("------------ MENU PRINCIPAL ----------");
+		System.out.println("  1 - Novo Jogo");
+		System.out.println("  2 - Exibir Historico de Vitorias");
+		System.out.println("  3 - Limpar Historico");
+		System.out.println("  4 - Creditos");
+		System.out.println("  0 - Sair");
+		System.out.println("---------------------------------------");
+		System.out.println("Digite a opção:");
+		int op = Integer.parseInt(scanner.nextLine());
+		return op;
+	}
+
+	public int escolherNivel(){
+		System.out.println("------------------------------------");
+		System.out.println("  Escolha o nivel:");
+		System.out.println("  1 - Facil");
+		System.out.println("  2 - Medio");
+		System.out.println("  3 - Dificil");
+		System.out.println("------------------------------------");
+		System.out.println("Digite a opção:");
+		int op = Integer.parseInt(scanner.nextLine());
+		return op;
 	}
 }
--- programacao_orientada_objetos/ProjetoFinal_AnaJulia_Nicole/src/Tabuleiro.java
+++ programacao_orientada_objetos/ProjetoFinal_AnaJulia_Nicole/src/Tabuleiro.java
@@ -1,5 +1,13 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Tabuleiro {
 	private char[][] tabuleiro;
@@ -7,8 +15,10 @@ public class Tabuleiro {
 	private int linha, coluna;
 	Random random = new Random();
 	Scanner scanner = new Scanner(System.in);
+	Date horaInicial, horaFinal;
 
-	public Tabuleiro(){
+	public Tabuleiro() {
+		horaInicial = Calendar.getInstance().getTime();
 		bombas = new int[10][10];
 		tabuleiro = new char[10][10];
 		iniciarBombas(); // coloca 0 em todas as posições do tabuleiro de bombas
@@ -18,76 +28,138 @@ public class Tabuleiro {
 
 	}
 
-	public boolean ganharJogo(){
+	public boolean ganharJogo() {
 
-		int count=0;
-		for(int linha = 1 ; linha < 9 ; linha++) {
-			for(int coluna = 1 ; coluna < 9 ; coluna++) {
-				if(tabuleiro[linha][coluna]=='#') {
+		int count = 0;
+		for (int linha = 1; linha < 9; linha++) {
+			for (int coluna = 1; coluna < 9; coluna++) {
+				if (tabuleiro[linha][coluna] == '#') {
 					count++;
 				}
 			}
 		}
-		if(count == 10) {
+//		if (count <= 50) {
+		if (count == 10) {			
 			return true;
-		}
-		else {
-			return false;                
+
+		} else {
+			return false;
 		}
 	}
 
-	public void abrirVizinhas(){
-		for(int i=-1 ; i<2 ; i++) {
-			for(int j=-1 ; j<2 ; j++) {
-				if((bombas[linha+i][coluna+j] != -1) && (linha != 0 && linha != 9 && coluna != 0 && coluna != 9)){
-					tabuleiro[linha+i][coluna+j]=Character.forDigit(bombas[linha+i][coluna+j], 10);
+	public void salvarHistorico(int resultado) {
+
+		String duracao = "";
+
+		try {
+			
+			FileWriter historico = new FileWriter("historico.txt", true);
+		
+			horaFinal = Calendar.getInstance().getTime();
+			long diff = horaFinal.getTime() - horaInicial.getTime();
+			long diffSec = diff / 1000 % 60;
+			long diffMin = diff / (60 * 1000) % 60;
+			long diffHoras = diff / (60 * 60 * 1000) % 24;
+			duracao = diffHoras + ":" + diffMin + ":" + diffSec;
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			
+			if (resultado == 1) {
+				historico.write(simpleDateFormat.format(horaInicial) + " | Nivel Facil | Duracao: " + duracao + " | Ganhou" + "\n");
+			} else {
+				historico.write(simpleDateFormat.format(horaInicial) + " | Nivel Facil | Duracao: " + duracao + " | Perdeu" + "\n");
+			}
+			
+			historico.close();
+
+		} catch (IOException ioe) {
+			System.err.println("IOException: " + ioe.getMessage());
+		}
+	}
+	
+	public void lerHistorico() {
+		
+		
+		 try {
+		      FileReader arquivo = new FileReader("historico.txt");
+		      BufferedReader lerArquivo = new BufferedReader(arquivo);
+
+		        System.out.printf("\n");
+
+		      
+		      // le a primeira linha, a variável "linha" recebe o valor "null" quando o 
+		      // processo de repetição atingir o final do arquivo texto		   
+		      String linha = lerArquivo.readLine();
+		      while (linha != null) {
+		        System.out.printf("%s\n", linha);
+		        linha = lerArquivo.readLine(); // lê da segunda até a última linha
+		      }
+
+		      arquivo.close();
+		      
+		    } catch (IOException e) {
+		    	System.err.printf("\nHistorico Vazio\n");
+//		        System.err.printf("Erro na abertura do historico: %s.\n",e.getMessage());
+		    }		
+	}
+
+	public void limparHistorico() {
+		
+	    File arquivo = new File("historico.txt"); 
+	    if (arquivo.delete()) { 
+	        System.out.printf("Historico limpado com sucesso!\n");
+	    }
+	}
+	
+	public void abrirVizinhas() {
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if ((bombas[linha + i][coluna + j] != -1) && (linha != 0 && linha != 9 && coluna != 0 && coluna != 9)) {
+					tabuleiro[linha + i][coluna + j] = Character.forDigit(bombas[linha + i][coluna + j], 10);
 				}
 			}
 		}
 	}
 
-	public int getPosicao(int linha, int coluna){
+	public int getPosicao(int linha, int coluna) {
 		return bombas[linha][coluna];
 	}
 
-	public boolean setPosicao(){
+	public boolean setPosicao() {
 
-		do{
-			System.out.print("\nLinha: "); 
+		do {
+			System.out.print("\nLinha: ");
 			linha = scanner.nextInt();
-			System.out.print("Coluna: "); 
+			System.out.print("Coluna: ");
 			coluna = scanner.nextInt();
 
-			if(linha < 1 || linha > 8 || coluna < 1 || coluna > 8) {
+			if (linha < 1 || linha > 8 || coluna < 1 || coluna > 8) {
 				System.out.println("Posicao Invalida!\nDigite numeros de 1 a 8");
-			}
-			else if (tabuleiro[linha][coluna] != '#') {
+			} else if (tabuleiro[linha][coluna] != '#') {
 				System.out.println("Essa posicao ja foi escolhida!");
 
 			}
 
-
-			//			if( (tabuleiro[linha][coluna] != '#') && ((linha < 9 && linha > 0) && (coluna < 9 && coluna > 0)))
-			//                System.out.println("Posicao ja escolhida!");
-			//            
-			//            if( linha < 1 || linha > 8 || coluna < 1 || coluna > 8)
-			//                System.out.println("Posicao Invalida!\nDigite numeros de 0 a 9");
-			//            
-
-
-
-
-			//			if((tabuleiro[linha][coluna] != '#') && ((linha < 9 && linha > 0) && (coluna < 9 && coluna > 0)))
-			//				System.out.println("Essa posicao ja foi escolhida!");
+			// if( (tabuleiro[linha][coluna] != '#') && ((linha < 9 && linha > 0) && (coluna
+			// < 9 && coluna > 0)))
+			// System.out.println("Posicao ja escolhida!");
 			//
-			//			if(linha < 0 || linha > 7 || coluna < 0 || coluna > 9) {
-			//				System.out.println("Posicao Invalida!\nDigite numeros de 0 a 9");
-			//			}
+			// if( linha < 1 || linha > 8 || coluna < 1 || coluna > 8)
+			// System.out.println("Posicao Invalida!\nDigite numeros de 0 a 9");
+			//
 
-		}while((linha < 1 && linha > 8) && (coluna < 1 && coluna > 8) || (tabuleiro[linha][coluna] != '#'));
-		//	}while((linha < 0 && linha > 7) && (coluna < 0 && coluna > 9) || (tabuleiro[linha][coluna] != '#'));
+			// if((tabuleiro[linha][coluna] != '#') && ((linha < 9 && linha > 0) && (coluna
+			// < 9 && coluna > 0)))
+			// System.out.println("Essa posicao ja foi escolhida!");
+			//
+			// if(linha < 0 || linha > 7 || coluna < 0 || coluna > 9) {
+			// System.out.println("Posicao Invalida!\nDigite numeros de 0 a 9");
+			// }
 
-		if(getPosicao(linha, coluna) == -1) {
+		} while ((linha < 1 && linha > 8) && (coluna < 1 && coluna > 8) || (tabuleiro[linha][coluna] != '#'));
+		// }while((linha < 0 && linha > 7) && (coluna < 0 && coluna > 9) ||
+		// (tabuleiro[linha][coluna] != '#'));
+
+		if (getPosicao(linha, coluna) == -1) {
 			return true;
 		} else {
 			return false;
@@ -95,8 +167,7 @@ public class Tabuleiro {
 
 	}
 
-	public void mostrarTabuleiro(){
-
+	public void mostrarTabuleiro() {
 
 //		System.out.print("\n   0 1 2 3 4 5 6 7 8 9"); 
 //		System.out.println("\n   -------------------"); 
@@ -126,50 +197,42 @@ public class Tabuleiro {
 //        System.out.println("\n            1   2   3   4   5   6   7   8");
 //        System.out.println("                      Colunas");
 
+		System.out.print("\n    1 2 3 4 5 6 7 8");
+		System.out.println("\n   ----------------");
 
-		System.out.print("\n    1 2 3 4 5 6 7 8"); 
-		System.out.println("\n   ----------------"); 
-		
-        for(int linha = 1 ; linha < 8 ; linha++){
-        	System.out.print(linha + " |");;
-            
-            for(int coluna = 1 ; coluna < 9 ; coluna++){
-                    System.out.print(" "+ tabuleiro[linha][coluna]);
-            }
-                
-            System.out.println();
-        }
-      
+		for (int linha = 1; linha < 8; linha++) {
+			System.out.print(linha + " |");
 
-
-
-		/*
-		System.out.print("\n   0 1 2 3 4 5 6 7 8 9"); 
-		System.out.println("\n   -------------------"); 
-
-
-//		for (int linha = 0; linha < 8; linha++){
-        for(int linha = 8 ; linha > 0 ; linha--){	
-			System.out.print(linha + " | ");
-
-//			for (int coluna = 0; coluna < 10; coluna++){
-			for (int coluna = 1; coluna < 9; coluna++){
-				System.out.print(" "+ tabuleiro[linha][coluna]);
+			for (int coluna = 1; coluna < 9; coluna++) {
+				System.out.print(" " + tabuleiro[linha][coluna]);
 			}
+
 			System.out.println();
 		}
+
+		/*
+		 * System.out.print("\n   0 1 2 3 4 5 6 7 8 9");
+		 * System.out.println("\n   -------------------");
+		 * 
+		 * 
+		 * // for (int linha = 0; linha < 8; linha++){ for(int linha = 8 ; linha > 0 ;
+		 * linha--){ System.out.print(linha + " | ");
+		 * 
+		 * // for (int coluna = 0; coluna < 10; coluna++){ for (int coluna = 1; coluna <
+		 * 9; coluna++){ System.out.print(" "+ tabuleiro[linha][coluna]); }
+		 * System.out.println(); }
 		 */
 	}
 
-	public void preencherInformacoesBombasVizinhas(){
+	public void preencherInformacoesBombasVizinhas() {
 
-		for (int linha=1 ; linha < 9 ; linha++) {
-			for(int coluna=1 ; coluna < 9 ; coluna++){
+		for (int linha = 1; linha < 9; linha++) {
+			for (int coluna = 1; coluna < 9; coluna++) {
 
-				for(int i=-1 ; i<=1 ; i++) {
-					for(int j=-1 ; j<=1 ; j++) {
-						if(bombas[linha][coluna] != -1) {
-							if(bombas[linha+i][coluna+j] == -1) {
+				for (int i = -1; i <= 1; i++) {
+					for (int j = -1; j <= 1; j++) {
+						if (bombas[linha][coluna] != -1) {
+							if (bombas[linha + i][coluna + j] == -1) {
 								bombas[linha][coluna]++;
 							}
 						}
@@ -179,50 +242,54 @@ public class Tabuleiro {
 		}
 	}
 
-	public void exibirTabuleiro(){
-		for(int i=1 ; i < 9; i++) {
-			for(int j=1 ; j < 9 ; j++) {
-				if(bombas[i][j] == -1) {
-					tabuleiro[i][j]='*';
+	public void exibirTabuleiro() {
+		for (int i = 1; i < 9; i++) {
+			for (int j = 1; j < 9; j++) {
+				if (bombas[i][j] == -1) {
+					tabuleiro[i][j] = '*';
+				} else {					
+					tabuleiro[i][j] = Character.forDigit(bombas[i][j], 10);
 				}
 			}
 		}
 		mostrarTabuleiro();
 	}
 
-	public void iniciaTabuleiro(){
-		for(int i=1; i < bombas.length; i++) {
-			for(int j=1; j < bombas.length; j++) {
-				tabuleiro[i][j]= '#';
+	public void iniciaTabuleiro() {
+		for (int i = 1; i < bombas.length; i++) {
+			for (int j = 1; j < bombas.length; j++) {
+				tabuleiro[i][j] = '#';
 			}
 		}
 	}
 
-	public void iniciarBombas(){
-		for(int i=0; i < bombas.length; i++) {
-			for(int j=0; j < bombas.length; j++) {
-				bombas[i][j]=0;
+	public void iniciarBombas() {
+		for (int i = 0; i < bombas.length; i++) {
+			for (int j = 0; j < bombas.length; j++) {
+				bombas[i][j] = 0;
 			}
 		}
 	}
 
-	public void sortearBombas(){
+	public void sortearBombas() {
+//	public void sortearBombas(int quantidadeBombas) {
 
 		boolean sorteado;
 		int linha, coluna;
-		for(int i=0 ; i<10 ; i++){
+//		for(int i=0; i < quantidadeBombas; i++){
+		for (int i = 0; i < 10; i++) {
 
-			do{
+			do {
 				linha = random.nextInt(8) + 1;
 				coluna = random.nextInt(8) + 1;
 
-				if(bombas[linha][coluna] == -1) {
-					sorteado=true;
+				if (bombas[linha][coluna] == -1) {
+					sorteado = true;
 				} else {
 					sorteado = false;
 				}
 
-			}while(sorteado);
+			} while (sorteado);
 
 			bombas[linha][coluna] = -1;
 		}
